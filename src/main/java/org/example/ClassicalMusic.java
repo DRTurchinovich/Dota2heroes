be15297e06b5e4aee8ee7ea7e114e101a1560ecd
DRTurchinovich <46638477+DRTurchinovich@users.noreply.github.com>
2022-10-06 22:54:59 +0300

    Add files via upload

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,9 +1,33 @@
 package org.example;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music{
 
     @Override
     public String getSong() {
-        return "Morgernshtern";
+        return "Morgenshtern";
     }
+
+//    private ClassicalMusic() {
+//
+//    }
+//
+//    public static ClassicalMusic getClassicalMusic() {
+//        return new ClassicalMusic();
+//    }
+//    public void doMyInit() {
+//        System.out.println("Doing my init");
+//    }
+//
+//    public void doMyDestroy() {
+//        System.out.println("Doing my destruction");
+//    }
+//
+//    @Override
+//    public String getSong() {
+//        return "Morgernshtern";
+//    }
 }
