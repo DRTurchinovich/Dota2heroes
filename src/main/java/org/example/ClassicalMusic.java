fc0262a39dd50448aab79008e76b004a445469ba
DRTurchinovich <turchinovichda@gmail.com>
2022-10-07 21:11:04 +0300

    Learned 10 11 12 course lessons

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,33 +1,30 @@
 package org.example;
 
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class ClassicalMusic implements Music{
+
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my init");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
 
     @Override
     public String getSong() {
         return "Morgenshtern";
     }
 
-//    private ClassicalMusic() {
-//
-//    }
-//
-//    public static ClassicalMusic getClassicalMusic() {
-//        return new ClassicalMusic();
-//    }
-//    public void doMyInit() {
-//        System.out.println("Doing my init");
-//    }
-//
-//    public void doMyDestroy() {
-//        System.out.println("Doing my destruction");
-//    }
-//
-//    @Override
-//    public String getSong() {
-//        return "Morgernshtern";
-//    }
 }
--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,40 +1,39 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
-    private Music music;
 
-    private String name;
-    private int volume;
+        @Value("${musicPlayer.name}")
+        private String name;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+        @Value("${musicPlayer.volume}")
+        private int volume;
+
+        private Music music1;
+        private Music music2;
+
+        @Autowired
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,@Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
-    public MusicPlayer() {
 
-    }
+    public String playMusic() {
+            return "Playing: " + music1.getSong() + ", " + music2.getSong();
+        }
 
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
-    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getVolume() {
         return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
     }
 }
